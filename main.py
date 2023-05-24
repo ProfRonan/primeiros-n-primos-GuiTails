599f1e85d7ec0866e4bb7c5664ecb4c5f6b3733a
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-05-24 10:51:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ O = F
 N = 2
 i = 1
 Lista = []
+Lista.append(2)
 if F <=0:
     print("Insira um número maior")
 if F > 0:
@@ -17,10 +18,12 @@ if F > 0:
         if N % i ==0 and i ==N:
             F = F - 1
             i = 1
-            Lista.append(N)
+            print(N)
+            Lista.append(f"{N}n")
             if F > 0:
                 N = N + 1
             if F <=0:
                 F = O
-                print((F),Lista)
+                Lista.remove("2n")
+                print(Lista)
                 break
